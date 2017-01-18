9ec2c868cd89ecc3335767e05a25812d196986b8
noahsherman <noahshermanmusic@gmail.com>
2017-01-18 16:00:06 -0500

    NBA_Python_Test
    Updated with removal of needless code, added comments

--- Hello_World.py
+++ Hello_World.py
@@ -3,15 +3,14 @@
 #This is me trying to pull NBA box score data from another text file
 
 import csv #imports csv functionality
-import re
 
 with open("/Users/noahsherman/Desktop/Python_NBA_Test.txt") as my_file:
     reader = csv.reader(my_file)
-    for line in reader:
+    for line in reader: #For each line in my_file
         if int(line[2]) >= 10 and int(line[3]) >= 10 and int(line[6]) >= 10:
-            name = str(line[0])
+            name = str(line[0]) #Turns first list item in to string, need this to remove useless info, see txt file.
             print "On Jan 17, %s had a triple double, with %s points, %s rebounds, and %s assists." %(name[0:-10], line[6], line[2], line[3])
-            break
+            #name[0:-10] removes name id that is pulled when copies from bref
         elif int(line[2]) >= 10 and int(line[3]) < 10 and int(line[6]) >= 10:
             name = str(line[0])
             print "On Jan 17, %s had a double double, with %s points and %s rebounds." %(name[0:-10], line[6], line[2])
@@ -21,10 +20,8 @@ with open("/Users/noahsherman/Desktop/Python_NBA_Test.txt") as my_file:
         else:
             pass
             
-#iterates through each line
-#if any of TRB, AST, STL, BLK, PTS == 0, player does not appear
 
-#Now try some if/else statements
+
 
 my_file.close()
 
